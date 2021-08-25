13385233add8e529aff6a2efc57ec3b05ac817f8
braginini <bangvalo@gmail.com>
2021-08-25 08:54:55 +0200

    chore: make navbar user menu squared

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -118,7 +118,7 @@ const Navbar = ({toggle}) => {
                                         leaveTo="transform opacity-0 scale-95"
                                     >
                                         <Menu.Items
-                                            className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            className="origin-top-right absolute right-0 mt-2 w-48 squared-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                                             <Menu.Item>
                                                 {({active}) => (
                                                     <NavLink
