6fad55b4cb853d425f5ac84444a2eb8829418aa3
Maycon Santos <mlsmaycon@gmail.com>
2021-12-20 16:45:33 +0100

    Fixx cron (#15)
    * fix crontab filename and expression
    
    * log to stdout

--- docker/init_cert.sh
+++ docker/init_cert.sh
@@ -13,10 +13,10 @@ fi
 # this also updates the nginx config file with new SSL entries
 certbot -n --nginx --agree-tos --email ${LETSENCRYPT_EMAIL} -d ${LETSENCRYPT_DOMAIN} --https-port ${NGINX_SSL_PORT}
 # Add cron job file
-cat <<EOF >/etc/crontabs/certbot
+cat <<EOF >/etc/crontabs/root
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-0 */12 * * * root certbot -q renew --nginx --https-port ${NGINX_SSL_PORT}
+0 */12 * * * certbot -q renew --nginx --https-port ${NGINX_SSL_PORT}
 EOF
 # start cron daemon
 supervisorctl start cron
