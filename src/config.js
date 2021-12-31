20c777edd47237cdb17befb3f274bb7b5863cac6
mlsmaycon <mlsmaycon@gmail.com>
2021-12-31 12:38:59 +0500

    remove comment

--- src/config.js
+++ src/config.js
@@ -7,7 +7,6 @@ if (process.env.NODE_ENV !== 'production') {
   configJson = require("./config.json");
 }
 
-// import configJson from configFile;
 export function getConfig() {
   // Configure the audience here. By default, it will take whatever is in the config
   // (specified by the `audience` key) unless it's the default value of "YOUR_API_IDENTIFIER" (which
