0af911cfcee95bc1ec7739f6295a77c8a1aa9e89
braginini <bangvalo@gmail.com>
2022-03-26 11:54:36 +0100

    Add announcement banner and new logo

---
+++ src/components/Banner.js
@@ -0,0 +1,51 @@
+import { XIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+
+export default function Banner() {
+	const [show, setShow] = useState(true);
+
+	const dismiss = () => {
+		setShow(false);
+	};
+
+	return show ? (
+		<div className="relative bg-indigo-300">
+			<div className="max-w-7xl mx-auto py-1 px-1 sm:px-6 lg:px-8">
+				<div className="pr-16 sm:text-center sm:px-16">
+					<p className="font-normal">
+						<span className="md:hidden">
+							Big news! Wiretrustee becomes <strong>netbird</strong>!
+						</span>
+						<span className="hidden md:inline">
+							Big news! Wiretrustee becomes <strong>netbird</strong>!
+						</span>
+						<span className="block sm:ml-2 sm:inline-block">
+							<a
+								href="https://blog.netbird.io/wiretrustee-becomes-netbird"
+								className="font-bold underline"
+								target="_blank"
+							>
+								{" "}
+								Learn more{" "}
+								<span aria-hidden="true">&rarr;</span>
+							</a>
+						</span>
+					</p>
+				</div>
+				<div className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
+					<button
+						type="button"
+						className="flex p-1 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white"
+						onClick={dismiss}
+					>
+						<span className="sr-only">Dismiss</span>
+						<XIcon
+							className="h-4 w-4"
+							aria-hidden="true"
+						/>
+					</button>
+				</div>
+			</div>
+		</div>
+	) : null;
+}
