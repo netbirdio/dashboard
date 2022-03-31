02ee40f2a9dee23071100ff8ce4a60171002f747
shatoboar <baesangwoo99@gmail.com>
2022-03-31 14:56:56 +0200

    Users empty page

--- src/views/Users.js
+++ src/views/Users.js
@@ -1,18 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 import Loading from "../components/Loading";
-import { deletePeer, getPeers } from "../api/ManagementAPI";
-import { timeAgo } from "../utils/common";
-import EditButton from "../components/EditButton";
-import CopyText from "../components/CopyText";
-import DeleteModal from "../components/DeleteDialog";
-import EmptyPeersPanel from "../components/EmptyPeers";
 
 export const Users = () => {
-    return (
-        <div> HELLO </div>
-    )
-}
+	return (
+		<>
+			<div className="py-10">
+				<header>
+					<div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+						<h1 className="text-2xl font-mono leading-tight text-gray-900 font-bold">
+							Users
+						</h1>
+					</div>
+				</header>
+				<main>
+					<div className="max-w-5xl mx-auto sm:px-6 lg:px-8">
+						<div className="px-4 py-8 sm:px-0">
+							<h1 className="text-m font-mono leading-tight text-gray-900 font-bold">
+                                Manage and review Users.
+							</h1>
+							<br />
+							<p className="text-sm font-mono">
+								Here you will see the users associated to this
+								Account. 
+							</p>
+							<br />
+							<p className="text-sm font-mono">Stay tuned.</p>
+						</div>
+					</div>
+				</main>
+			</div>
+		</>
+	);
+};
 
 export default withAuthenticationRequired(Users, {
 	onRedirecting: () => <Loading />,
