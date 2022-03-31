b242732592e8164811d3a47a90e2d4f1776c3ccb
braginini <bangvalo@gmail.com>
2022-03-31 16:45:02 +0200

    Add new styles [NOT FINISHED]

--- src/views/Peers.js
+++ src/views/Peers.js
@@ -115,7 +115,7 @@ export const Peers = () => {
 		<>
 			<div className="py-10">
 				<header>
-					<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+					<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
 						<h1 className="text-2xl leading-tight text-gray-900 font-normal">
 							Peers
 						</h1>
@@ -126,7 +126,7 @@ export const Peers = () => {
 				</header>
 				<main>
 					<div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-						<div className="px-4 py-8 sm:px-0">
+						<div className="px-4 sm:px-0">
 							{loading && <Loading />}
 							{error != null && <span>{error.toString()}</span>}
 							<main>
