ae48f73acca8954d8fe39d4c14581cd7211621c5
braginini <bangvalo@gmail.com>
2022-05-08 23:10:12 +0200

    Add SetupKey filter and search

--- src/views/SetupKeys.js
+++ src/views/SetupKeys.js
@@ -12,6 +12,7 @@ import NewSetupKeyDialog from "../components/NewSetupKeyDialog";
 export const SetupKeysComponent = () => {
 
         const [setupKeys, setSetupKeys] = useState([])
+        const [setupKeysBackUp, setSetupKeysBackUp] = useState([]);
         const [loading, setLoading] = useState(true)
         const [error, setError] = useState(null)
         const [showDeleteDialog, setShowDeleteDialog] = useState(false)
@@ -20,6 +21,59 @@ export const SetupKeysComponent = () => {
         const [deleteDialogTitle, setDeleteDialogTitle] = useState("")
         const [keyToRevoke, setKeyToRevoke] = useState(null)
 
+        const handleSearch = (e) => {
+            let tempArray = setupKeysBackUp.filter((item) => {
+                return item.Name.toUpperCase().includes(e.toUpperCase())
+            });
+            setSetupKeys(tempArray);
+        };
+
+        const showValid = () => {
+            const showValidBtn = document.getElementById("btn-show-valid");
+            const showAllBtn = document.getElementById("btn-show-all");
+
+            showValidBtn.classList.add(
+                "ring-1",
+                "ring-indigo-500",
+                "border-indigo-500",
+                "outline-none"
+            );
+            showAllBtn.classList.remove(
+                "ring-1",
+                "ring-indigo-500",
+                "border-indigo-500",
+                "outline-none"
+            );
+            refresh(validFilter);
+        };
+
+        const validFilter = function (keys) {
+            return keys.filter((key) => {
+                return key.Valid;
+            });
+        }
+
+        const showAll = () => {
+            const showAllBtn = document.getElementById("btn-show-all");
+            const showValidBtn = document.getElementById("btn-show-valid");
+
+            showAllBtn.classList.add(
+                "ring-1",
+                "ring-indigo-500",
+                "border-indigo-500",
+                "outline-none"
+            );
+            showValidBtn.classList.remove(
+                "ring-1",
+                "ring-indigo-500",
+                "border-indigo-500",
+                "outline-none"
+            );
+
+            refresh(null)
+        };
+
+
         const handleNewKeyClick = () => {
             setShowNewKeyDialog(true)
         }
@@ -27,7 +81,7 @@ export const SetupKeysComponent = () => {
         const newSetupKeyDialogCallback = (cancelled, name, type, expiresIn) => {
             if (!cancelled) {
                 createSetupKey(getAccessTokenSilently, name, type, expiresIn)
-                    .then(() => refresh())
+                    .then(() => refresh(validFilter))
                     .catch(error => {
                         console.log(error)
                     })
@@ -61,7 +115,7 @@ export const SetupKeysComponent = () => {
             if (confirmed && !keyToRevoke.Revoked) {
                 revokeSetupKey(getAccessTokenSilently, keyToRevoke.Id)
                     .then(() => setKeyToRevoke(null))
-                    .then(() => refresh())
+                    .then(() => refresh(validFilter))
                     .catch(error => {
                         setKeyToRevoke(null)
                         console.log(error)
@@ -71,41 +125,34 @@ export const SetupKeysComponent = () => {
             }
         }
 
-        const refresh = () => {
+        const refresh = (filter) => {
             getSetupKeys(getAccessTokenSilently)
                 .then(responseData => responseData.sort((a, b) => (a.Name > b.Name) ? 1 : -1))
-                .then(sorted => setSetupKeys(sorted))
+                .then((sorted) => {
+                    return filter != null ? filter(sorted) : sorted;
+                })
+                .then((filtered) => {
+                    setSetupKeysBackUp(filtered);
+                    setSetupKeys(filtered);
+                })
                 .then(() => setLoading(false))
                 .catch(error => handleError(error))
         }
 
         useEffect(() => {
-            refresh()
+            refresh(validFilter)
         }, [getAccessTokenSilently])
 
         return (
             <>
                 <div className="py-10 bg-gray-50 overflow-hidden rounded max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <header className="sm:flex sm:items-center">
-                        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 sm:flex-auto">
-                            <h1 className="text-xl font-semibold text-gray-900">Setup Keys</h1>
+                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:flex-auto">
+                            <h1 className="text-xl font-semibold text-gray-900">Peers</h1>
                             <p className="mt-2 text-sm text-gray-700">
                                 A list of all the setup keys in your account including their name, state, type and
                                 expiration.
                             </p>
-                        </div>
-                        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 sm:flex-auto mt-2 sm:mt-0 sm:ml-16 sm:flex-none">
-
-
-                            <button
-                                type="button"
-                                className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                                onClick={() => {
-                                    handleNewKeyClick()
-                                }}
-                            >
-                                Add Key
-                            </button>
                         </div>
                     </header>
                     <main>
@@ -115,7 +162,54 @@ export const SetupKeysComponent = () => {
                         )}
                         {setupKeys && (
                             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                                <div className="px-4 py-8 sm:px-0">
+                                <div className="auto py-8">
+                                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-6 gap-5">
+                                        <div className="lg:col-span-2">
+                                            <input
+                                                className="text-sm w-full rounded p-2 border border-gray-300 focus:border-gray-400 outline-none"
+                                                placeholder="Search..."
+                                                type="search"
+                                                onChange={(e) => handleSearch(e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="lg:col-span-2"/>
+                                        <div className="flex lg:justify-end justify-center">
+                                            <div className="flex items-center">
+                                                  <span className="relative z-0 inline-flex shadow-sm rounded-md">
+                                                    <button
+                                                        id="btn-show-valid"
+                                                        onClick={() => showValid()}
+                                                        type="button"
+                                                        className="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 z-10 outline-none ring-1 ring-indigo-500 border-indigo-500"
+                                                    >
+                                                      Valid
+                                                    </button>
+                                                    <button
+                                                        type="button"
+                                                        id="btn-show-all"
+                                                         onClick={() => showAll()}
+                                                        className="relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 outline-none hover:bg-gray-50"
+                                                    >
+                                                      All
+                                                    </button>
+                                                  </span>
+                                            </div>
+                                        </div>
+                                        <div className="lg:flex lg:justify-end">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                                                onClick={() => {
+                                                    handleNewKeyClick()
+                                                }}
+                                            >
+                                                Add Key
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="px-4 py-2 sm:px-0">
                                     <DeleteModal show={showDeleteDialog}
                                                  confirmCallback={handleRevokeConfirmation}
                                                  text={deleteDialogText} title={deleteDialogTitle}/>
@@ -124,7 +218,8 @@ export const SetupKeysComponent = () => {
                                     <div className="flex flex-col">
                                         <div className="-my-2 sm:-mx-6 lg:-mx-8">
                                             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                                                <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                                <div
+                                                    className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                                                     <table className="min-w-full divide-y divide-gray-200">
                                                         <thead className="bg-gray-50">
                                                         <tr>
@@ -163,12 +258,14 @@ export const SetupKeysComponent = () => {
                                                                 <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{setupKey.Name}</td>
                                                                 <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                                                     {setupKey.Valid && (
-                                                                        <span className="inline-flex rounded-full bg-green-100 px-2 text-xs leading-5 text-green-800">
+                                                                        <span
+                                                                            className="inline-flex rounded-full bg-green-100 px-2 text-xs leading-5 text-green-800">
                                                                         valid
                                                                     </span>
                                                                     )}
                                                                     {!setupKey.Valid && (
-                                                                        <span className="inline-flex rounded-full bg-red-100 px-2 text-xs leading-5 text-red-800">
+                                                                        <span
+                                                                            className="inline-flex rounded-full bg-red-100 px-2 text-xs leading-5 text-red-800">
                                                                          {setupKey.State}
                                                                     </span>
                                                                     )}
