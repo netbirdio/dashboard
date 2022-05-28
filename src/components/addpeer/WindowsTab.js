68b17bd07eb93dd0b3d3804c1957ad63635dd0c0
Maycon Santos <mlsmaycon@gmail.com>
2022-05-28 11:03:10 +0200

    update v0.6.1 (#48)

--- src/components/addpeer/WindowsTab.js
+++ src/components/addpeer/WindowsTab.js
@@ -16,7 +16,7 @@ const WindowsTab = ({setupKey}) => {
             icon: ArrowCircleRightIcon,
             iconBackground: 'bg-gray-600',
             content: <button type="button"
-                             onClick={() => window.open("https://github.com/netbirdio/netbird/releases/latest/download/netbird_installer_0.6.0_windows_amd64.exe")}
+                             onClick={() => window.open("https://github.com/netbirdio/netbird/releases/latest/download/netbird_installer_0.6.1_windows_amd64.exe")}
                              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                 Download Netbird
             </button>,
