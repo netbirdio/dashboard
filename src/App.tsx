aebe6ca82be4d7e2822ffff44b76c1938653a546
mlsmaycon <mlsmaycon@gmail.com>
2022-06-01 21:00:36 +0200

    Temporally disable ACLs and Activity routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import FooterComponent from './components/FooterComponent';
 import Loading from "./components/Loading";
 import SetupKeys from "./views/SetupKeys";
 import AddPeer from "./views/AddPeer";
-import AccessControl from "./views/AccessControl";
-import Activity from "./views/Activity";
+// import AccessControl from "./views/AccessControl";
+// import Activity from "./views/Activity";
 import Users from './views/Users';
 import Banner from "./components/Banner";
 import {store} from "./store";
@@ -89,8 +89,8 @@ function App() {
                             <Route path='/peers' exact component={Peers}/>
                             <Route path="/add-peer" component={AddPeer}/>
                             <Route path="/setup-keys" component={SetupKeys}/>
-                            <Route path="/acls" component={AccessControl}/>
-                            <Route path="/activity" component={Activity}/>
+                            {/*<Route path="/acls" component={AccessControl}/>*/}
+                            {/*<Route path="/activity" component={Activity}/>*/}
                             <Route path="/users" component={Users}/>
                         </Switch>
                     </Content>
