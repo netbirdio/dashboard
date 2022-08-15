8e4d60f487fd0598262486a51cb199ce05f1de73
braginini <bangvalo@gmail.com>
2022-08-15 16:27:04 +0200

    Add local ru nscripts

---
+++ run-local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker rm -f netbird-dashboard
+docker run -d --name netbird-dashboard --rm -p 3000:80 -p 443:443 -e AUTH_AUDIENCE=http://localhost:3000/ -e AUTH_AUTHORITY=https://netbird-localdev.eu.auth0.com -e AUTH_CLIENT_ID=kBRMAOqIZ7hvpVCaypQLCJvTzkYYIXVt -e USE_AUTH0=true -e AUTH_SUPPORTED_SCOPES='openid profile email api offline_access email_verified' -e NETBIRD_MGMT_API_ENDPOINT=http://localhost:80 -e NETBIRD_MGMT_GRPC_API_ENDPOINT=http://localhost:80  wiretrustee/dashboard:oidc
