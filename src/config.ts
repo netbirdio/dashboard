c375f872990a9736c938b0e06b9080d12832f8ad
braginini <bangvalo@gmail.com>
2022-08-15 16:22:10 +0200

    Add backwards compatibility for the config

--- src/config.ts
+++ src/config.ts
@@ -8,16 +8,6 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export function getConfig() {
-  // Configure the audience here. By default, it will take whatever is in the config
-  // (specified by the `audience` key) unless it's the default value of "YOUR_API_IDENTIFIER" (which
-  // is what you get sometimes by using the Auth0 sample download tool from the quickstart page, if you
-  // don't have an API).
-  // If this resolves to `null`, the API page changes to show some helpful info about what to do
-  // with the audience.
-  const audience =
-    configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
-      ? configJson.audience
-      : null;
 
   return {
     auth0Auth: configJson.auth0Auth,
@@ -27,6 +17,6 @@ export function getConfig() {
     apiOrigin: configJson.apiOrigin,
     grpcApiOrigin: configJson.grpcApiOrigin,
     latestVersion: configJson.latestVersion,
-    ...(audience ? { audience } : null),
+    audience: configJson.authAudience,
   };
 }
