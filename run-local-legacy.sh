46904963f24a9ac6225375f0a13a46ec2e45239d
braginini <bangvalo@gmail.com>
2022-08-15 17:10:26 +0200

    Fix run local scripts

--- run-local-legacy.sh
+++ run-local-legacy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
+MGMT_PORT=$1
+
 npm run build
 docker build -f docker/Dockerfile -t netbird/dashboard-local:latest .
 
-docker run -d --name netbird-dashboard -p 3000:80 -p 443:443 -e AUTH0_AUDIENCE=http://localhost:3000/ -e AUTH0_DOMAIN=netbird-localdev.eu.auth0.com -e AUTH0_CLIENT_ID=kBRMAOqIZ7hvpVCaypQLCJvTzkYYIXVt -e NETBIRD_MGMT_API_ENDPOINT=http://localhost:80 -e NETBIRD_MGMT_GRPC_API_ENDPOINT=http://localhost:80  netbird/dashboard-local:latest
+docker rm -f netbird-dashboard
+docker run -d --name netbird-dashboard  \
+ -p 3000:80 -p 443:443  \
+ -e AUTH0_AUDIENCE=http://localhost:3000/ \
+ -e AUTH0_DOMAIN=netbird-localdev.eu.auth0.com  \
+ -e AUTH0_CLIENT_ID=kBRMAOqIZ7hvpVCaypQLCJvTzkYYIXVt  \
+ -e NETBIRD_MGMT_API_ENDPOINT=http://localhost:$MGMT_PORT  \
+ -e NETBIRD_MGMT_GRPC_API_ENDPOINT=http://localhost:$MGMT_PORT  \
+ netbird/dashboard-local:latest
