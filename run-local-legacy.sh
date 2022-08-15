ef3b99b05d269d96df4de88e5483013795fbccce
braginini <bangvalo@gmail.com>
2022-08-15 16:56:31 +0200

    Make run local scripts complete

--- run-local-legacy.sh
+++ run-local-legacy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker rm -f netbird-dashboard
-docker run -d --name netbird-dashboard -p 3000:80 -p 443:443 -e AUTH0_AUDIENCE=http://localhost:3000/ -e AUTH0_DOMAIN=netbird-localdev.eu.auth0.com -e AUTH0_CLIENT_ID=kBRMAOqIZ7hvpVCaypQLCJvTzkYYIXVt -e NETBIRD_MGMT_API_ENDPOINT=http://localhost:80 -e NETBIRD_MGMT_GRPC_API_ENDPOINT=http://localhost:80  wiretrustee/dashboard:oidc
+npm run build
+docker build -f docker/Dockerfile -t netbird/dashboard-local:latest .
+
+docker run -d --name netbird-dashboard -p 3000:80 -p 443:443 -e AUTH0_AUDIENCE=http://localhost:3000/ -e AUTH0_DOMAIN=netbird-localdev.eu.auth0.com -e AUTH0_CLIENT_ID=kBRMAOqIZ7hvpVCaypQLCJvTzkYYIXVt -e NETBIRD_MGMT_API_ENDPOINT=http://localhost:80 -e NETBIRD_MGMT_GRPC_API_ENDPOINT=http://localhost:80  netbird/dashboard-local:latest
