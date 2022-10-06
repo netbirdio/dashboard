225d07d47783c8e9471d79b4d7ded0601717bbec
braginini <bangvalo@gmail.com>
2022-10-06 16:57:16 +0200

    Fix Expires display when the date is after 2100

--- src/store/setup-key/sagas.ts
+++ src/store/setup-key/sagas.ts
@@ -56,7 +56,6 @@ export function* saveSetupKey(action: ReturnType<typeof actions.saveSetupKey.req
         const newGroups = [...keyToSave.auto_groups, ...resGroups]
         let effect
         if (!keyToSave.id) {
-            console.log(keyToSave)
             effect = yield call(service.createSetupKey, {
                 getAccessTokenSilently: action.payload.getAccessTokenSilently,
                 payload: {
