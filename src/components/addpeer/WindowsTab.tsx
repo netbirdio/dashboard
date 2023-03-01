ffa7fff4004c18dc315661f955bc5407419ada84
Maycon Santos <mlsmaycon@gmail.com>
2023-03-01 15:00:36 +0100

    Use new windows release URL (#144)
    removed latestVersion logic and environment support

--- src/components/addpeer/WindowsTab.tsx
+++ src/components/addpeer/WindowsTab.tsx
@@ -3,18 +3,15 @@ import React, {useState} from 'react';
 import { Button } from "antd";
 import TabSteps from "./TabSteps";
 import { StepCommand } from "./types"
-import {getConfig} from "../../config";
-const {latestVersion} = getConfig();
 
 export const WindowsTab = () => {
 
-    const releaseVersion = latestVersion ? latestVersion.replace("v", "") : "0.6.3"
     const [steps, setSteps] = useState([
         {
             key: 1,
             title: 'Download and run Windows installer:',
             commands: (
-                <Button type="primary" href={`https://github.com/netbirdio/netbird/releases/download/v${releaseVersion}/netbird_installer_${releaseVersion}_windows_amd64.exe`} target="_blank">Download NetBird</Button>
+                <Button type="primary" href="https://pkgs.netbird.io/windows/x64" target="_blank">Download NetBird</Button>
             ),
             copied: false
         } as StepCommand,
