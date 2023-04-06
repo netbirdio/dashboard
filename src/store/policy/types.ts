f413a020175c8bdae4774445c1b447e39c7c88b3
Givi Khojanashvili <gigovich@gmail.com>
2023-04-06 17:26:40 +0400

    Modify policy to handle rules

--- src/store/policy/types.ts
+++ src/store/policy/types.ts
@@ -1,7 +1,29 @@
+import { Group } from "../group/types";
+
+export interface PolicyRule {
+    id?: string
+    name: string
+    description: string
+    enabled: boolean
+    sources: Group[] | string[] | null
+    destinations: Group[] | string[] | null
+    flow: string
+    action: string
+    protocol: string
+    ports: string[]
+}
+
 export interface Policy {
     id?: string
     name: string
+    description: string
+    enabled: boolean
+    query: string
+    rules: PolicyRule[]
 };
 
-export interface PolicyToSave {
+export interface PolicyToSave extends Policy {
+    sourcesNoId: string[],
+    destinationsNoId: string[],
+    groupsToSave: string[]
 };
