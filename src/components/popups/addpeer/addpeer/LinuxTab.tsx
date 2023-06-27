79c58d7434bcf4acf70a4a918e9b3364cd73d814
Sarooj Bukhari <120650489+saroojbukhari2022@users.noreply.github.com>
2023-06-27 09:38:35 +0500

    add copy button on add peer code box and remove check to shoe defauilt rule

--- src/components/popups/addpeer/addpeer/LinuxTab.tsx
+++ src/components/popups/addpeer/addpeer/LinuxTab.tsx
@@ -1,28 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 import { Button } from "antd";
 import TabSteps from "./TabSteps";
-import { StepCommand } from "./types"
-
+import { StepCommand } from "./types";
 
 export const OtherTab = () => {
+  const [steps, _] = useState([
+    {
+      key: 1,
+      title: "For other installation options check our documentation.",
+      commands: (
+        <Button
+          type="primary"
+          href={`https://docs.netbird.io/how-to/getting-started#binary-install`}
+          target="_blank"
+        >
+          Documentation
+        </Button>
+      ),
+      copied: false,
+    } as StepCommand,
+  ]);
 
-    const [steps, _] = useState([
-        {
-            key: 1,
-            title: 'For other installation options check our documentation.',
-            commands: (
-                <Button type="primary" href={`https://docs.netbird.io/how-to/getting-started#binary-install`} target="_blank">
-                    Documentation
-                </Button>
-            ),
-            copied: false,
-        } as StepCommand,
-    ])
+  return <TabSteps stepsItems={steps} />;
+};
 
-    return (
-        <TabSteps stepsItems={steps} />
-    )
-}
-
-export default OtherTab
+export default OtherTab;
--- src/components/popups/addpeer/addpeer/types.ts
+++ src/components/popups/addpeer/addpeer/types.ts
@@ -1,9 +1,10 @@
 import * as React from "react";
 
 export interface StepCommand {
-    key: number | string,
-    title: React.ReactNode | string | null,
-    commands: React.ReactNode | string | null,
-    copied?: boolean,
-    showCopyButton?: boolean
+  key: number | string;
+  title: React.ReactNode | string | null;
+  commands: React.ReactNode | string | null;
+  commandsForCopy?: React.ReactNode | string | null;
+  copied?: boolean;
+  showCopyButton?: boolean;
 }
