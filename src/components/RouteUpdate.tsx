1fe96b889b986c7b8348aa47737f9ee4a43e55ee
Pascal Fischer <pascal@netbird.io>
2023-11-11 12:10:22 +0100

    disallow routing through ios devices

--- src/components/RouteUpdate.tsx
+++ src/components/RouteUpdate.tsx
@@ -96,6 +96,7 @@ const RouteAddNew = () => {
       !os.toLowerCase().startsWith("darwin") &&
       !os.toLowerCase().startsWith("windows") &&
       !os.toLowerCase().startsWith("android") &&
+      !os.toLowerCase().startsWith("ios") &&
       route &&
       !routes
         .filter((r) => r.network_id === route.network_id)
