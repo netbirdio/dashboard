4612f6c49a97afa12396c8d7f6399bbcec858f77
Eduard Gert <kontakt@eduardgert.de>
2024-01-24 18:36:20 +0100

    Add "New dashboard" Banner (#315)
    * Add new dashboard Banner.tsx
    
    * display on localdev and add todo note
    
    ---------
    
    Co-authored-by: Maycon Santos <mlsmaycon@gmail.com>

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -15,18 +15,20 @@ const Banner = () => {
 		localStorage.setItem(banner_closed_key,'true');
 	};
 
-	const announcement = "NetBird is now available on the App Store."
+	// todo: when updating this announcement, ensure to
+	// remove the condition on App.tsx that limits the current banner to cloud and local dev
+	const announcement = "Discover the new NetBird Dashboard with a brand new look and feel."
 
 	const announcement_md5 = Md5.hashStr(announcement)
 
 	const linkLearnMore = () => {
 		return (
 			<a
-				href="https://apps.apple.com/us/app/netbird-p2p-vpn/id6469329339"
+				href="https://preview.netbird.io/peers"
 				className="font-bold underline"
 				target="_blank"
 				rel="noreferrer"
-			><Text strong style={{color: "#ffffff"}}>Download&nbsp;<span aria-hidden="true">&rarr;</span></Text></a>
+			><Text strong style={{color: "#ffffff"}}>Try it out now&nbsp;<span aria-hidden="true">&rarr;</span></Text></a>
 		)
 	}
 
