54e66d461cf7e376bb9bf33629a7abe883289cf9
Eduard Gert <kontakt@eduardgert.de>
2024-02-02 17:11:19 +0100

    Add iOS operating system icon

--- src/hooks/useOperatingSystem.ts
+++ src/hooks/useOperatingSystem.ts
@@ -12,7 +12,7 @@ export const getOperatingSystem = (os: string) => {
   if (os.includes("darwin")) return OperatingSystem.APPLE as const;
   if (os.includes("mac")) return OperatingSystem.APPLE as const;
   if (os.includes("android")) return OperatingSystem.ANDROID as const;
-  if (os.includes("ios")) return OperatingSystem.APPLE as const;
+  if (os.includes("ios")) return OperatingSystem.IOS as const;
   if (os.includes("win")) return OperatingSystem.WINDOWS as const;
   return OperatingSystem.LINUX as const;
 };
