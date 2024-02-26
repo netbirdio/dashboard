eef5ada591dc3dc7bb183cce0ccf136e2fe7b6bc
Eduard Gert <kontakt@eduardgert.de>
2024-02-26 14:31:06 +0100

    Add peer detail view skeleton loader

---
+++ src/components/skeletons/SkeletonPeerDetail.tsx
@@ -0,0 +1,40 @@
+import Skeleton from "react-loading-skeleton";
+
+type Props = {
+  withHeader?: boolean;
+};
+
+export default function SkeletonPeerDetail({ withHeader = true }: Props) {
+  return (
+    <div className={"w-full mt-6 p-default"}>
+      <div className={"flex flex-wrap w-full justify-between max-w-6xl "}>
+        <Skeleton height={24} width={300} className={"rounded-md"} />
+      </div>
+      <div className={"flex flex-wrap w-full justify-between mt-4 max-w-6xl "}>
+        <Skeleton height={42} width={400} className={"rounded-md"} />
+        <div className={"flex gap-3"}>
+          <Skeleton height={42} width={80} className={"rounded-md"} />
+          <Skeleton height={42} width={120} className={"rounded-md"} />
+        </div>
+      </div>
+      <div
+        className={
+          "flex flex-wrap w-full justify-between mt-6 max-w-6xl gap-10"
+        }
+      >
+        <Skeleton
+          height={400}
+          width={"100%"}
+          className={"rounded-md"}
+          containerClassName={"flex-1 "}
+        />
+        <Skeleton
+          height={300}
+          width={"100%"}
+          className={"rounded-md opacity-30"}
+          containerClassName={"flex-1 "}
+        />
+      </div>
+    </div>
+  );
+}
