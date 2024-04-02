0ba012d98aac9c55413014b7b3d048022b1ed695
Eduard Gert <kontakt@eduardgert.de>
2024-04-02 11:35:43 +0200

    Remove integrations

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -2,21 +2,21 @@
 
 import { ScrollArea } from "@components/ScrollArea";
 import { cn } from "@utils/helpers";
-import { isLocalDev, isNetBirdHosted } from "@utils/netbird";
 import { CustomFlowbiteTheme, Sidebar } from "flowbite-react";
 import { SidebarItemGroupProps } from "flowbite-react/lib/esm/components/Sidebar/SidebarItemGroup";
 import AccessControlIcon from "@/assets/icons/AccessControlIcon";
 import ActivityIcon from "@/assets/icons/ActivityIcon";
 import DNSIcon from "@/assets/icons/DNSIcon";
 import DocsIcon from "@/assets/icons/DocsIcon";
-import IntegrationIcon from "@/assets/icons/IntegrationIcon";
 import NetworkRoutesIcon from "@/assets/icons/NetworkRoutesIcon";
 import PeerIcon from "@/assets/icons/PeerIcon";
 import SettingsIcon from "@/assets/icons/SettingsIcon";
 import SetupKeysIcon from "@/assets/icons/SetupKeysIcon";
 import TeamIcon from "@/assets/icons/TeamIcon";
 import SidebarItem from "@/components/SidebarItem";
+import { useAnnouncement } from "@/contexts/AnnouncementProvider";
 import { useLoggedInUser } from "@/contexts/UsersProvider";
+import { headerHeight } from "@/layouts/Header";
 
 const customTheme: CustomFlowbiteTheme["sidebar"] = {
   root: {
@@ -34,6 +34,8 @@ export default function Navigation({
   hideOnMobile = false,
 }: Props) {
   const { isUser } = useLoggedInUser();
+  const { isOwnerOrAdmin } = useLoggedInUser();
+  const { bannerHeight } = useAnnouncement();
 
   return (
     <Sidebar
@@ -42,123 +44,133 @@ export default function Navigation({
         hideOnMobile ? "hidden md:block" : "",
         fullWidth
           ? "w-auto max-w-[22rem]"
-          : "w-[15rem] min-w-[15rem] overflow-y-auto",
+          : "w-[15rem] max-w-[15rem] min-w-[15rem] overflow-y-auto",
       )}
       theme={customTheme}
       style={{
-        height: fullWidth ? "calc(100vh - 75px)" : "100%",
+        height: fullWidth
+          ? `calc(100vh - ${headerHeight + bannerHeight}px)`
+          : "100%",
       }}
     >
-      <Sidebar.Items className={cn(fullWidth ? "w-10/12" : "fixed")}>
+      <Sidebar.Items className={cn(fullWidth ? "w-10/12" : "fixed h-full")}>
         <ScrollArea
           style={{
-            height: !fullWidth ? "calc(100vh - 75px)" : "100%",
+            height: !fullWidth
+              ? `calc(100vh - ${headerHeight + bannerHeight}px)`
+              : "100%",
           }}
-          className={"pt-4"}
         >
-          <SidebarItemGroup>
-            <SidebarItem icon={<PeerIcon />} label="Peers" href={"/peers"} />
-
-            {!isUser && (
-              <>
+          <div
+            className={
+              "flex flex-col justify-between pt-4 w-[15rem] max-w-[15rem] min-w-[15rem]"
+            }
+            style={{
+              height: !fullWidth
+                ? `calc(100vh - ${headerHeight + bannerHeight}px)`
+                : "100%",
+            }}
+          >
+            <div>
+              <SidebarItemGroup>
                 <SidebarItem
-                  icon={<SetupKeysIcon />}
-                  label="Setup Keys"
-                  href={"/setup-keys"}
+                  icon={<PeerIcon />}
+                  label="Peers"
+                  href={"/peers"}
                 />
-                <SidebarItem
-                  icon={<AccessControlIcon />}
-                  label="Access Control"
-                  collapsible
-                >
+
+                {!isUser && (
+                  <>
+                    <SidebarItem
+                      icon={<SetupKeysIcon />}
+                      label="Setup Keys"
+                      href={"/setup-keys"}
+                    />
+                    <SidebarItem
+                      icon={<AccessControlIcon />}
+                      label="Access Control"
+                      collapsible
+                    >
+                      <SidebarItem
+                        label="Policies"
+                        href={"/access-control"}
+                        isChild
+                        exactPathMatch={true}
+                      />
+                      <SidebarItem
+                        label="Posture Checks"
+                        isChild
+                        href={"/posture-checks"}
+                        exactPathMatch={true}
+                      />
+                    </SidebarItem>
+
+                    <SidebarItem
+                      icon={<NetworkRoutesIcon />}
+                      label="Network Routes"
+                      href={"/network-routes"}
+                    />
+                    <SidebarItem
+                      icon={<DNSIcon />}
+                      label="DNS"
+                      collapsible
+                      exactPathMatch={true}
+                    >
+                      <SidebarItem
+                        label="Nameservers"
+                        isChild
+                        href={"/dns/nameservers"}
+                      />
+                      <SidebarItem
+                        label="DNS Settings"
+                        isChild
+                        href={"/dns/settings"}
+                      />
+                    </SidebarItem>
+                    <SidebarItem icon={<TeamIcon />} label="Team" collapsible>
+                      <SidebarItem label="Users" isChild href={"/team/users"} />
+                      <SidebarItem
+                        label="Service Users"
+                        isChild
+                        href={"/team/service-users"}
+                      />
+                    </SidebarItem>
+                    <SidebarItem
+                      icon={<ActivityIcon />}
+                      label="Activity"
+                      href={"/activity"}
+                    />
+                  </>
+                )}
+
+                {isUser && (
                   <SidebarItem
-                    label="Policies"
-                    href={"/access-control"}
-                    isChild
+                    icon={<DocsIcon />}
+                    href={"https://docs.netbird.io/"}
+                    target={"_blank"}
+                    label="Documentation"
+                  />
+                )}
+              </SidebarItemGroup>
+              {!isUser && (
+                <SidebarItemGroup>
+                  <SidebarItem
+                    icon={<SettingsIcon />}
+                    label="Settings"
+                    href={"/settings"}
                     exactPathMatch={true}
                   />
-                  <SidebarItem
-                    label="Posture Checks"
-                    isChild
-                    href={"/posture-checks"}
-                    exactPathMatch={true}
-                  />
-                </SidebarItem>
 
-                <SidebarItem
-                  icon={<NetworkRoutesIcon />}
-                  label="Network Routes"
-                  href={"/network-routes"}
-                />
-                <SidebarItem
-                  icon={<DNSIcon />}
-                  label="DNS"
-                  collapsible
-                  exactPathMatch={true}
-                >
                   <SidebarItem
-                    label="Nameservers"
-                    isChild
-                    href={"/dns/nameservers"}
+                    icon={<DocsIcon />}
+                    href={"https://docs.netbird.io/"}
+                    target={"_blank"}
+                    label="Documentation"
                   />
-                  <SidebarItem
-                    label="DNS Settings"
-                    isChild
-                    href={"/dns/settings"}
-                  />
-                </SidebarItem>
-                <SidebarItem icon={<TeamIcon />} label="Team" collapsible>
-                  <SidebarItem label="Users" isChild href={"/team/users"} />
-                  <SidebarItem
-                    label="Service Users"
-                    isChild
-                    href={"/team/service-users"}
-                  />
-                </SidebarItem>
-                <SidebarItem
-                  icon={<ActivityIcon />}
-                  label="Activity"
-                  href={"/activity"}
-                />
-              </>
-            )}
-
-            {isUser && (
-              <SidebarItem
-                icon={<DocsIcon />}
-                href={"https://docs.netbird.io/"}
-                target={"_blank"}
-                label="Documentation"
-              />
-            )}
-          </SidebarItemGroup>
-
-          {!isUser && (
-            <SidebarItemGroup>
-              <SidebarItem
-                icon={<SettingsIcon />}
-                label="Settings"
-                href={"/settings"}
-                exactPathMatch={true}
-              />
-
-              {(isLocalDev() || isNetBirdHosted()) && (
-                <SidebarItem
-                  icon={<IntegrationIcon />}
-                  label="Integrations"
-                  href={"/integrations"}
-                  exactPathMatch={true}
-                />
+                </SidebarItemGroup>
               )}
-
-              <SidebarItem
-                icon={<DocsIcon />}
-                href={"https://docs.netbird.io/"}
-                target={"_blank"}
-                label="Documentation"
-              />
-            </SidebarItemGroup>
-          )}
+            </div>
+          </div>
         </ScrollArea>
       </Sidebar.Items>
     </Sidebar>
@@ -167,7 +179,10 @@ export default function Navigation({
 
 export function SidebarItemGroup(props: SidebarItemGroupProps) {
   return (
-    <Sidebar.ItemGroup className={"dark:border-zinc-700/40"} {...props}>
+    <Sidebar.ItemGroup
+      className={"dark:border-zinc-700/40 space-y-1.5"}
+      {...props}
+    >
       {props.children}
     </Sidebar.ItemGroup>
   );
