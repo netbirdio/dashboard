974f3f691efb285ad4a42928b0aaa9b6170daa26
Maycon Santos <mlsmaycon@gmail.com>
2024-06-06 16:52:19 +0200

    Fix http://localhost:3000/ url validation case

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -60,7 +60,7 @@ export const validator = {
   isValidUrl: (urlString: string) => {
     const urlPattern = new RegExp(
       "^(https?:\\/\\/)?" + // validate protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{1,}|localhost|" + // validate domain name
         "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
         "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
         "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
