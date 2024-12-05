994b526b9b60ad1a641274455484fc5b079fe9e6
Eduard Gert <kontakt@eduardgert.de>
2024-12-05 16:26:22 +0100

    Prevent text selection of the input placeholder

--- src/components/DropdownInput.tsx
+++ src/components/DropdownInput.tsx
@@ -20,7 +20,7 @@ export const DropdownInput = forwardRef<HTMLInputElement, Props>(
             "min-h-[42px] w-full relative",
             "border-b-0 border-t-0 border-r-0 border-l-0 border-neutral-200 dark:border-nb-gray-700 items-center",
             "bg-transparent text-sm outline-none focus-visible:outline-none ring-0 focus-visible:ring-0",
-            "dark:placeholder:text-nb-gray-400 font-light placeholder:text-neutral-500 pl-10",
+            "dark:placeholder:text-nb-gray-400 font-light placeholder:text-neutral-500 pl-10 select-none",
           )}
           value={value}
           onChange={(e) => onChange(e.target.value)}
