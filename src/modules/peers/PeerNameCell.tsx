5e56e0697c402d04112d4546aee3702e22af9048
Vladislav Tropnikov <vladislav.tropnikov@genestack.com>
2025-03-21 14:40:20 +0100

    Add more possible id parameters

--- src/modules/peers/PeerNameCell.tsx
+++ src/modules/peers/PeerNameCell.tsx
@@ -40,7 +40,7 @@ export default function PeerNameCell({ peer, linkToPeer = true }: Props) {
           }
         >
           <div className={"text-nb-gray-400 font-light truncate"}>
-            {!userOfPeer?.email && userOfPeer?.id}
+            {userOfPeer?.email || userOfPeer?.name || peer?.user_id || userOfPeer?.id}
           </div>
         </ActiveInactiveRow>
       </div>
