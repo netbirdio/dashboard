ac478f0b5def2e32076d2530f8f718da8be69817
Eduard Gert <kontakt@eduardgert.de>
2025-06-05 10:56:50 +0200

    Update wording for dns domain, macOS and Windows install steps

--- src/modules/setup-netbird-modal/WindowsTab.tsx
+++ src/modules/setup-netbird-modal/WindowsTab.tsx
@@ -48,7 +48,7 @@ export default function WindowsTab({
           {GRPC_API_ORIGIN && (
             <Steps.Step step={2}>
               <p>
-                {`Click on "Settings" from the NetBird icon in your system tray and enter the following "Management URL"`}
+                {`Click on "Settings" then "Advanced Settings" from the NetBird icon in your system tray and enter the following "Management URL"`}
               </p>
               <Code>
                 <Code.Line>{GRPC_API_ORIGIN}</Code.Line>
