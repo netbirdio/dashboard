1b7e8832e4511989b621c4a834a9c3dbe39c1b69
Eduard Gert <kontakt@eduardgert.de>
2025-10-20 16:18:12 +0200

    Adjust group name cell

--- src/modules/groups/GroupsNameCell.tsx
+++ src/modules/groups/GroupsNameCell.tsx
@@ -1,42 +1,42 @@
 import { GroupBadgeIcon } from "@components/ui/GroupBadgeIcon";
 import TextWithTooltip from "@components/ui/TextWithTooltip";
-import { cn } from "@utils/helpers";
+import { useRouter } from "next/navigation";
 import React from "react";
 import CircleIcon from "@/assets/icons/CircleIcon";
 import { Group } from "@/interfaces/Group";
-import { useRouter } from "next/navigation";
 
 type Props = {
   active: boolean;
   group: Group;
 };
 export default function GroupsNameCell({ active, group }: Readonly<Props>) {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <div className={cn("gap-3 dark:text-neutral-300 text-neutral-500 min-w-0")}>
-      <div className={"flex flex-col gap-1"}>
-        <div className={"flex gap-2.5 items-center"}>
-          <div className={"flex items-center justify-center h-full"}>
-            <GroupBadgeIcon id={group?.id} issued={group?.issued} />
-          </div>
-
-          <div className={"flex flex-col min-w-0 cursor-pointer"}
-            aria-label={`View details of group ${group.name}`}
-            onClick={() => router.push("/group?id=" + group.id)}
-          >
-            <div
-              className={"font-medium flex gap-2 items-center justify-center"}
-            >
-              <TextWithTooltip text={group?.name} maxChars={25} />
-            </div>
-          </div>
-          <CircleIcon
-            size={8}
-            active={active}
-            inactiveDot={"gray"}
-            className={"shrink-0"}
-          />
+    <div className={""}>
+      <div
+        className={
+          "inline-flex items-center justify-start text-neutral-300 gap-2.5 py-2 px-3 pr-4 hover:bg-nb-gray-800/60 cursor-pointer rounded-md"
+        }
+      >
+        <div className={"flex items-center justify-center h-full"}>
+          <GroupBadgeIcon id={group?.id} issued={group?.issued} />
         </div>
+
+        <div
+          className={"flex flex-col min-w-0 cursor-pointer"}
+          aria-label={`View details of group ${group.name}`}
+          onClick={() => router.push("/group?id=" + group.id)}
+        >
+          <div className={"font-medium flex gap-2 items-center justify-center"}>
+            <TextWithTooltip text={group?.name} maxChars={50} />
+          </div>
+        </div>
+        <CircleIcon
+          size={8}
+          active={active}
+          inactiveDot={"gray"}
+          className={"shrink-0"}
+        />
       </div>
     </div>
   );
