27f4b27f01a1f10126193168b4e5ba7e89f9f30a
Eduard Gert <kontakt@eduardgert.de>
2025-10-31 17:07:25 +0100

    Fix group name click

--- src/modules/groups/table/GroupsNameCell.tsx
+++ src/modules/groups/table/GroupsNameCell.tsx
@@ -17,6 +17,7 @@ export default function GroupsNameCell({ active, group }: Readonly<Props>) {
         className={
           "inline-flex items-center justify-start text-neutral-300 gap-2.5 py-2 px-3 pr-4 hover:bg-nb-gray-800/60 cursor-pointer rounded-md"
         }
+        onClick={() => router.push("/group?id=" + group.id)}
       >
         <div className={"flex items-center justify-center h-full"}>
           <GroupBadgeIcon id={group?.id} issued={group?.issued} />
@@ -25,7 +26,6 @@ export default function GroupsNameCell({ active, group }: Readonly<Props>) {
         <div
           className={"flex flex-col min-w-0 cursor-pointer"}
           aria-label={`View details of group ${group.name}`}
-          onClick={() => router.push("/group?id=" + group.id)}
         >
           <div className={"font-medium flex gap-2 items-center justify-center"}>
             <TextWithTooltip text={group?.name} maxChars={50} />
