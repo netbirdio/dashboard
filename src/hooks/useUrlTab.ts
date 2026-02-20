d63db3ac8416a215f0037d63a5ac3868aeb83523
Eduard Gert <kontakt@eduardgert.de>
2026-02-20 13:50:13 +0100

    Prevent nextjs navigation trigger on tab change

--- src/hooks/useUrlTab.ts
+++ src/hooks/useUrlTab.ts
@@ -1,27 +1,34 @@
-import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback, useMemo } from "react";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
 
 export default function useUrlTab(
   validTabs: string[],
   defaultTab: string,
 ): [string, (value: string) => void] {
   const searchParams = useSearchParams();
-  const router = useRouter();
 
-  const tab = useMemo(() => {
-    const tabParam = searchParams.get("tab");
-    if (tabParam && validTabs.includes(tabParam)) return tabParam;
-    return defaultTab;
-  }, [searchParams, validTabs, defaultTab]);
-
-  const setTab = useCallback(
-    (value: string) => {
-      const params = new URLSearchParams(searchParams.toString());
-      params.set("tab", value);
-      router.replace(`?${params.toString()}`, { scroll: false });
+  const getTab = useCallback(
+    (params: URLSearchParams) => {
+      const tabParam = params.get("tab");
+      if (tabParam && validTabs.includes(tabParam)) return tabParam;
+      return defaultTab;
     },
-    [searchParams, router],
+    [validTabs, defaultTab],
   );
+
+  const [tab, setTabState] = useState(() => getTab(searchParams));
+
+  useEffect(() => {
+    const newTab = getTab(searchParams);
+    setTabState(newTab);
+  }, [searchParams, getTab]);
+
+  const setTab = useCallback((value: string) => {
+    setTabState(value);
+    const params = new URLSearchParams(window.location.search);
+    params.set("tab", value);
+    window.history.replaceState(null, "", `?${params.toString()}`);
+  }, []);
 
   return [tab, setTab];
 }
