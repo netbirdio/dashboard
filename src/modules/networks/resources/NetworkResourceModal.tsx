1ef7de07312b0e308e785dc2d75f8f35941dd915
Misha Bragin <bangvalo@gmail.com>
2026-03-12 20:46:56 +0100

    Improve text for optional resource setiings

--- src/modules/networks/resources/NetworkResourceModal.tsx
+++ src/modules/networks/resources/NetworkResourceModal.tsx
@@ -309,7 +309,7 @@ export function ResourceModalContent({
                   }
                 >
                   <span className={"relative top-[1px]"}>
-                    Additional Options (optional)
+                    Optional Settings
                   </span>
                 </AccordionTrigger>
                 <AccordionContent className={""}>
