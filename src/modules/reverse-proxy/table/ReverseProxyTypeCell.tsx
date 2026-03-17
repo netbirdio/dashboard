911b43f51d7f9d3c9b611799e113491f2504b469
Eduard Gert <kontakt@eduardgert.de>
2026-03-17 13:40:45 +0100

    Move proxy type icon into type component

--- src/modules/reverse-proxy/table/ReverseProxyTypeCell.tsx
+++ src/modules/reverse-proxy/table/ReverseProxyTypeCell.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import { useMemo } from "react";
-import { ReverseProxy } from "@/interfaces/ReverseProxy";
-import { ReverseProxyServiceIcon } from "@/modules/reverse-proxy/ReverseProxyServiceIcon";
+import { ReverseProxy, ServiceMode } from "@/interfaces/ReverseProxy";
 import { trim } from "lodash";
 import { SERVICE_MODES } from "@/modules/reverse-proxy/ReverseProxyServiceModeSelector";
 import Badge from "@components/Badge";
+import { cn } from "@utils/helpers";
+import { ArrowRightFromLineIcon, GlobeIcon, LockKeyhole } from "lucide-react";
 
 type Props = {
   reverseProxy?: ReverseProxy;
@@ -30,4 +31,41 @@ export const ReverseProxyTypeCell = ({ reverseProxy }: Props) => {
       </Badge>
     </div>
   );
+};
+
+type ReverseProxyServiceIconProps = {
+  reverseProxy?: ReverseProxy;
+  className?: string;
+  size?: number;
+};
+
+export const ReverseProxyServiceIcon = ({
+  reverseProxy,
+  className,
+  size = 14,
+}: ReverseProxyServiceIconProps) => {
+  const mode = reverseProxy?.mode;
+
+  switch (mode) {
+    case ServiceMode.HTTP:
+      return <GlobeIcon size={size} className={cn("shrink-0", className)} />;
+    case ServiceMode.TLS:
+      return <LockKeyhole size={size} className={cn("shrink-0", className)} />;
+    case ServiceMode.TCP:
+      return (
+        <ArrowRightFromLineIcon
+          size={size}
+          className={cn("shrink-0", className)}
+        />
+      );
+    case ServiceMode.UDP:
+      return (
+        <ArrowRightFromLineIcon
+          size={size}
+          className={cn("shrink-0", className)}
+        />
+      );
+    default:
+      return <GlobeIcon size={size} className={cn("shrink-0", className)} />;
+  }
 };
